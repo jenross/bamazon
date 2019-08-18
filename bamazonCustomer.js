21121f13e0f9233ef4eaddd5c77e49c9617bc0a8
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-08-18 15:55:06 -0400

    initial inquirer prompt running; grabbing id user inputs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,12 +21,47 @@ connection.connect(function(err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
   console.log("We are connected");
-  runBamazon(); 
+  showAllProducts(); 
 });
 
-// function runBamazon() {
-//     inquirer
-//       .prompt({
-//         name: "idOfProduct",
-       
-//   }
+function showAllProducts() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log("------------------------------------------------------------------------------------");
+      console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + "$" + res[i].price + " | " + res[i].stock_quantity);
+    }
+    console.log("************************************************************************************");
+  });
+  whatToBuy();
+}
+
+function whatToBuy() {
+  connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw err;
+
+    inquirer
+      .prompt([
+        {
+        name: "productID",
+        type: "number",
+        message: "Enter the ID# for the product you would like to buy"
+        },
+        {
+        name: "units",
+        type: "number",
+        message: "How many units of the product would you like to buy?"
+        }
+      ])
+      .then(function(answer) {
+        // get the information of the chosen item
+        let chosenProduct;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].id === answer.productID) {
+            chosenProduct = results[i];
+          }
+        }
+      })
+    })
+    connection.end();
+}
