1e8c0a20d0a431560793a2898424c4017a590fde
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-08-18 11:07:36 -0400

    created initial mysql database and start of js file for connecting

---
+++ bamazonCustomer.js
@@ -0,0 +1,32 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+// create the connection information for the sql database
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon_db"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  console.log("We are connected");
+  runBamazon(); 
+});
+
+// function runBamazon() {
+//     inquirer
+//       .prompt({
+//         name: "idOfProduct",
+       
+//   }
---
+++ bamazon_db.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(175) NOT NULL,
+  department_name VARCHAR(50) NOT NULL,
+  price DECIMAL(10,2) NULL,
+  stock_quantity INT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("blink xt2", "smart home", 99.99, 15000),
+       ("kitchenaid mixer", "home & kitchen", 335.29, 2500), 
+       ("bose soundlink headphones", "headphones", 195.18, 10500), 
+       ("paper mate felt tip pens", "office & school supplies", 9.96, 25600),
+       ("echo dot", "smart home", 29.99, 28300), 
+       ("apple airpods", "headphones", 144.52, 1500), 
+       ("martin smith ukulele", "guitars", 19.99, 1300), 
+       ("jameson guitars 5-string banjo", 154.95, 4200), 
+       ("samsung 50-inch smart tv", "television & video", 397.99, 675), 
+       ("ring doorbell", "smart home", 99.99, 9400);
