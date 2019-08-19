ef410697df56fa05d2a045c3d50d19c45c5ffc06
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-08-19 13:35:23 -0400

    menu options showing and view products working(manager)

--- bamazonManager.js
+++ bamazonManager.js
@@ -32,50 +32,58 @@ function menuOptions() {
         .prompt([
           {
           name: "options",
-          type: "rawList",
-          choices: "[]"
+          type: "list",
+          choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"], 
+          message: "What would you like to do?"
           },
-          {
-          name: "units",
-          type: "number",
-          message: "How many would you like to buy?"
-          }
         ])
         .then(function(answer) {
           // get the information of the chosen item
-          let chosenProduct;
-          for (var i = 0; i < results.length; i++) {
-            if (results[i].id === answer.productID) {
-              chosenProduct = results[i];
-            }
-          }
-          if (chosenProduct.stock_quantity < answer.units) {
-            console.log("Insufficient quantity!");
-          } else {
-            console.log("Congratulations, your order was placed!");
-            updatedStockQuantity = chosenProduct.stock_quantity - answer.units;
-            // console.log(updatedStockQuantity);
-            connection.query(
-              "UPDATE products SET ? WHERE ?",
-              [
-                {
-                  stock_quantity: updatedStockQuantity 
-                },
-                {
-                  product_name: chosenProduct.product_name
-                }
-              ],
-              function(err) {
-                if (err) throw err;
-                // console.log(res.affectedRows + " products updated!\n");
-                connection.end();
+          console.log(answer);
+          if(answer.options === 'View Products for Sale') {
+            connection.query("SELECT * FROM products", function(err, res) {
+              if (err) throw err;
+              for (var i = 0; i < res.length; i++) {
+                console.log("-------------------------------------------------------------------------------------------");
+                console.log("ID: " + res[i].id + " | " + "Product: " + res[i].product_name + " | " + "Dept: " + res[i].department_name + " | " + "$" + res[i].price + " | " + "# In-Stock: " + res[i].stock_quantity);
               }
-            );
-            // logs the actual query being run
-            // console.log(query.sql);
-            let cost = answer.units * chosenProduct.price; 
-            console.log("Your total cost is: " + "$" + cost);
+              console.log("************************************************************************************");
+            });
           }
+          // let chosenProduct;
+          // for (var i = 0; i < results.length; i++) {
+          //   if (results[i].id === answer.productID) {
+          //     chosenProduct = results[i];
+          //   }
+          // }
+      //     if (chosenProduct.stock_quantity < answer.units) {
+      //       console.log("Insufficient quantity!");
+      //     } else {
+      //       console.log("Congratulations, your order was placed!");
+      //       updatedStockQuantity = chosenProduct.stock_quantity - answer.units;
+      //       // console.log(updatedStockQuantity);
+      //       connection.query(
+      //         "UPDATE products SET ? WHERE ?",
+      //         [
+      //           {
+      //             stock_quantity: updatedStockQuantity 
+      //           },
+      //           {
+      //             product_name: chosenProduct.product_name
+      //           }
+      //         ],
+      //         function(err) {
+      //           if (err) throw err;
+      //           // console.log(res.affectedRows + " products updated!\n");
+      //           connection.end();
+      //         }
+      //       );
+      //       // logs the actual query being run
+      //       // console.log(query.sql);
+      //       let cost = answer.units * chosenProduct.price; 
+      //       console.log("Your total cost is: " + "$" + cost);
+      //     }
         })
-      })
+      });
 }
+
